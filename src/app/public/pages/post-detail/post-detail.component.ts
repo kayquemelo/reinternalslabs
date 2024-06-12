d9628c52cf824eb6532e844aa11a048d85807a29
kayque melo <mellokayck@gmail.com>
2024-06-11 21:09:48 -0300

    add:post detail

--- src/app/public/pages/post-detail/post-detail.component.ts
+++ src/app/public/pages/post-detail/post-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, Subscription, interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-post-detail',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrl: './post-detail.component.css'
 })
 export class PostDetailComponent {
+
+  private subscriptions: Subscription = new Subscription();
+  
+  public bgImage: string = 'assets/digitization_bg.jpg';
+
+  public readonly bgImageList: Array<string> = [
+    'assets/ai_lampada_bg.jpg',
+    'assets/bright_bg.jpg',
+    'assets/digitization_bg.jpg',
+    'assets/cyborg_bg.png',
+  ];
+
+  ngOnInit(): void {
+    this.subscriptions.add(
+      this.getPseudoRandomBgImagePath().subscribe(index => {
+        this.bgImage = this.bgImageList[index];
+      })
+    );
+  }
+
+  public getPseudoRandomBgImagePath(): Observable<number> {
+    return interval(5000).pipe(
+      map(() => Math.floor(Math.random() * this.bgImageList.length))
+    );
+  }
+
+  public scrollToDomElementID(element: HTMLElement): void {
+    element.scrollIntoView({ behavior: "smooth" });
+  }
 
 }
