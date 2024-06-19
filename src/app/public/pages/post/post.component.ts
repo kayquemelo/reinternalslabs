02e13e16115512b9faf4ea8ed3b1478de34e587f
kayque melo <mellokayck@gmail.com>
2024-06-18 21:26:07 -0300

    add: novos cards

--- src/app/public/pages/post/post.component.ts
+++ src/app/public/pages/post/post.component.ts
@@ -14,6 +14,8 @@ export class PostComponent implements OnInit {
     { id: "002", title: "SQLite do Zero - part. 2", subtitle: "Preparando o ambiente", date: "2024-06-02", author: "Kayque Melo" },
     { id: "003", title: "SQLite do Zero - part. 3", subtitle: "Quebrando a solução em partes", date: "2024-06-02", author: "Kayque Melo" },
     { id: "004", title: "SQLite do Zero - part. 4", subtitle: "Implementação e documentação", date: "2024-06-02", author: "Kayque Melo" },
+    { id: "005", title: "SQLite do Zero - part. 5", subtitle: "Implementação e codificação", date: "2024-06-02", author: "Kayque Melo" },
+    { id: "006", title: "SQLite do Zero - part. 6", subtitle: "Implementação e codificação", date: "2024-06-02", author: "Kayque Melo" },
   ];
 
   constructor(private _router: Router) {}
